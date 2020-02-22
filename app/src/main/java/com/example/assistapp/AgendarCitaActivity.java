7f708ce6683a8d77d5a5edf49604be5142dc831e
jggarciago <jggarciago@gmail.com>
2020-02-22 18:14:13 -0500

    Limpiar métodos anonimos

--- app/src/main/java/com/example/assistapp/AgendarCitaActivity.java
+++ app/src/main/java/com/example/assistapp/AgendarCitaActivity.java
@@ -24,6 +24,7 @@ public class AgendarCitaActivity extends AppCompatActivity {
 
     private Button agendarBtn1;
     private EditText tipoTxt1, duracionTxt1, horaTxt1, estadoTxt1;
+    ProgressDialog loading;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,36 +43,41 @@ public class AgendarCitaActivity extends AppCompatActivity {
                 agendarCita();
             }
         });
+        loading = null;
+
     }
 
     private void agendarCita(){
-        final ProgressDialog loading = ProgressDialog.show(this, "Por favor espere...",
+        loading = ProgressDialog.show(this, "Por favor espere...",
                 "Actualizando datos...",false, false);
 
-
+        //Extract pls
         Map<String, String> params = new HashMap<String, String>();
         params.put("tipoServicio", tipoTxt1.getText().toString());
         params.put("duracion", duracionTxt1.getText().toString());
         params.put("hora", horaTxt1.getText().toString());
         params.put("estado", estadoTxt1.getText().toString());
 
-        Consumidor.getInstance().agendarCita(this,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        loading.dismiss();
-                        Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    //La respuesta es en formato JSON
-                    public void onErrorResponse(VolleyError error) {
-                        loading.dismiss();
-                        Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
-                    }
-                }, params);
+        Consumidor.getInstance().agendarCita(this, onResponse, onError, params);
     }
+
+    private Response.Listener onResponse = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            loading.dismiss();
+            Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+        }
+    };
+
+    private Response.ErrorListener onError = new Response.ErrorListener() {
+        @Override
+        //La respuesta es en formato JSON
+        public void onErrorResponse(VolleyError error) {
+            loading.dismiss();
+            loading.dismiss();
+            Toast.makeText(getApplicationContext(), error.toString(), Toast.LENGTH_LONG).show();
+        }
+    };
 
 }
 
